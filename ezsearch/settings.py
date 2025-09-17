1f14db6af8605e2136e32f08d450fa10869e6027
JamesYon <trickymoon01@gmail.com>
2025-09-17 03:00:21 -0400

    add PostgreSQL

--- ezsearch/settings.py
+++ ezsearch/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'ezsearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ezsearch',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
